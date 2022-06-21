61062abf41f847bf1d7b00e41fa61d398e2e60d9
Sergey <tootoo97@gmail.com>
2022-06-21 20:54:25 +0300

    add validateParams to handlers

--- infrastructure/handlers/transactions.go
+++ infrastructure/handlers/transactions.go
@@ -2,41 +2,46 @@ package handlers
 
 import (
 	"net/http"
+
 	"github.com/labstack/echo/v4"
 )
 
-type  transactionHandler struct{
-Service TransactionService
+type transactionHandler struct {
+	Service TransactionService
 }
 
-func NewTransactionHandler(Service TransactionService)*transactionHandler{
+func NewTransactionHandler(Service TransactionService) *transactionHandler {
+	if Service == nil {
+		panic("Service is nil")
+	}
 	return &transactionHandler{Service: Service}
 }
 
-func(t *transactionHandler)AddToBalance(ctx echo.Context)error{
-	req :=new(AddToBalanceRequest)
-	err:=ctx.Bind(&req)
-	if err!=nil{
-		return echo.NewHTTPError(http.StatusBadRequest,"Wrong input data")
+func (t *transactionHandler) AddToBalance(ctx echo.Context) error {
+	req := new(AddToBalanceRequest)
+	err := ctx.Bind(&req)
+
+	if err != nil||!ValidateParams(req.Amount, req.UserId){
+		return echo.NewHTTPError(http.StatusBadRequest, "Wrong input data")
 	}
-	//todo validateParam
-	if err:=t.Service.AddToBalance(req.UserId, req.Amount); err!=nil{
-		return echo.NewHTTPError(400,"")
+
+	if err := t.Service.AddToBalance(req.UserId, req.Amount); err != nil {
+		return echo.NewHTTPError(400, err.Error())
 	}
 
 	return nil
 }
 
-func(t *transactionHandler)AddTransfer(ctx echo.Context)error{
-	req :=new(AddTransferRequest)
-	err:=ctx.Bind(&req)
-	if err!=nil{
-		return echo.NewHTTPError(http.StatusBadRequest,"Wrong input data")
-	}
-	//todo validateParam
-	if err:=t.Service.AddTransfer(req.FromId, req.ToId, req.Amount); err!=nil{
-		return echo.NewHTTPError(400,"")
+func (t *transactionHandler) AddTransfer(ctx echo.Context) error {
+	req := new(AddTransferRequest)
+	err := ctx.Bind(&req)
+
+	if err != nil||!ValidateParams(req.Amount, req.FromId,req.ToId) {
+		return echo.NewHTTPError(http.StatusBadRequest, "Wrong input data")
 	}
 
+	if err := t.Service.AddTransfer(req.FromId, req.ToId, req.Amount); err != nil {
+		return echo.NewHTTPError(400, err.Error())
+	}
 	return nil
 }
