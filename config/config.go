a8c5b4de92dcbe0633f0d4bf35996df7e4628926
Sergey <tootoo97@gmail.com>
2022-06-21 20:09:13 +0300

    add database config from env

---
+++ config/config.go
@@ -0,0 +1,43 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type DBConfig struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	DBName   string
+}
+
+type Config struct {
+	DBConfig
+}
+
+func NewConfig() *Config {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+	return &Config{
+		DBConfig: DBConfig{
+			Host:     getEnv("DB_HOST", ""),
+			Port:     getEnv("DB_PORT", ""),
+			User:     getEnv("DB_USER_NAME", ""),
+			Password: getEnv("DB_PASSWORD", ""),
+			DBName:   getEnv("DB_NAME", ""),
+		},
+	}
+}
+
+func getEnv(key string, defaultVal string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+	return defaultVal
+}
