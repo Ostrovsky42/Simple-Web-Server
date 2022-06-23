4e1ce56187b151b043b3ac49e418dce3dd80647a
Sergey <tootoo97@gmail.com>
2022-06-23 16:43:25 +0300

    add loger ro transactionServise

--- app/transaction_service.go
+++ app/transaction_service.go
@@ -1,50 +1,54 @@
 package app
 
 import (
-	"SimplyWebServer/infrastructure/handlers"
 	"SimplyWebServer/library"
-	"errors"
+	"github.com/sirupsen/logrus"
 )
 
 type transactionService struct {
 	Storage Storage
+	log     *logrus.Logger
 }
 
-func NewTransactionService(Storage Storage) *transactionService {
-	if Storage==nil{
-		panic("Storage is nil")
+func NewTransactionService(Storage Storage, l *logrus.Logger) *transactionService {
+	if Storage == nil {
+		l.Fatal("Storage is nil")
 	}
-	return &transactionService{Storage: Storage}
-}
-
-func (t *transactionService) AddToBalance(userId int, amount float32)error{
-
-	if err:=t.Storage.AddToBalance(userId, amount);err!=nil{
-		return err
+	return &transactionService{
+		Storage: Storage,
+		log:     l,
 	}
-	if err:=t.Storage.AddTransaction(library.Adding,userId,amount);err!=nil{
-		return err
-	}
-	return nil
-	}
-
-func (t *transactionService) AddTransfer(fromId int,toId int, amount float32)error {
-	if !handlers.ValidateParams(amount, fromId, toId){
-		return errors.New("400")
-	}
-	if err:=t.Storage.AddToBalance(fromId,-amount);err!=nil{
-		return err
-	}
-	if err:=t.Storage.AddToBalance(toId,amount);err!=nil{
-		return err
-	}
-	if err:=t.Storage.AddTransaction(library.Transaction,fromId,-amount);err!=nil{
-		return err
-	}
-	if err:=t.Storage.AddTransaction(library.Transaction,toId,amount);err!=nil{
-		return err
-	}
-	return nil
 }
 
 
+func (t *transactionService) AddToBalance(userId int, amount float32) (float32, error) {
+
+	balance, err := t.Storage.UpdateBalanceByUserId(userId, amount)
+	if err != nil {
+		t.log.Error("UpdateBalanceByUserId")
+		return 0, err
+	}
+	if err := t.Storage.AddTransaction(library.Adding, userId, amount); err != nil {
+		return 0, err
+	}
+	return balance, nil
+}
+
+func (t *transactionService) AddTransfer(fromId int, toId int, amount float32) (float32, float32, error) {
+
+	fromBalance, err := t.Storage.UpdateBalanceByUserId(fromId, -amount)
+	if err != nil {
+		return 0, 0, err
+	}
+	toBalance, err := t.Storage.UpdateBalanceByUserId(toId, amount)
+	if err != nil {
+		return 0, 0, err
+	}
+	if err := t.Storage.AddTransaction(library.Transaction, fromId, -amount); err != nil {
+		return 0, 0, err
+	}
+	if err := t.Storage.AddTransaction(library.Transaction, toId, amount); err != nil {
+		return 0, 0, err
+	}
+	return fromBalance, toBalance, nil
+}
